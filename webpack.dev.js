2dfd10c836525bd1591d5e65f35ec5f58d354004
Belov Roman <rnbelov96@gmail.com>
2020-11-06 07:53:23 +0300

    Обновляет конфигурацию Webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,67 +1,25 @@
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack')
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.js')
 
-module.exports = {
-  entry: ['./src/index.tsx'],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle-[hash].js',
-  },
+module.exports = merge(common, {
   devtool: 'inline-source-map',
+
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 9000,
     watchContentBase: true,
     progress: true,
+    open: true,
+    historyApiFallback: true,
+    hot: true,
   },
+
   mode: 'development',
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx', 'json'],
-    alias: {
-      components: path.resolve(__dirname, 'src/components/'),
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.(sass|scss|css)$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(png|jpg|gif|webp)$/,
-        use: ['file-loader'],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ['file-loader'],
-      },
-      {
-        test: /\.html$/,
-        use: ['html-loader'],
-      },
-    ],
-  },
+
   plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      inject: false,
-      template: require('html-webpack-template'),
-      title: 'Arrange Game App',
-      mobile: true,
-      lang: 'en-US',
-      appMountId: 'root',
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name]-[hash].css',
-    }),
+    new webpack.HotModuleReplacementPlugin(),
   ],
-};
+});
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,88 +1,23 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.js')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCssPlugin = require('optimize-css-assets-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
-module.exports = {
-  entry: ['./src/index.tsx'],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle-[hash].js',
-  },
+module.exports = merge(common, {
   mode: 'production',
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx', 'json'],
-    alias: {
-      components: path.resolve(__dirname, 'src/components/'),
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.(sass|scss|css)$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(png|jpg|gif|webp)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8192,
-            },
-          },
-          'image-webpack-loader',
-        ],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ['file-loader'],
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-            options: {
-              minimize: true,
-            },
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      inject: false,
-      template: require('html-webpack-template'),
-      title: 'Arrange Game App',
-      mobile: true,
-      lang: 'en-US',
-      appMountId: 'root',
-      minify: {
-        collapseWhitespace: true,
-      },
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name]-[hash].css',
-    }),
-  ],
+
+  devtool: false,
+
   optimization: {
     minimizer: [
-      new OptimizeCssPlugin(),
-      new TerserPlugin({
-        cache: true,
-        parallel: true,
-      }),
+      new CssMinimizerPlugin(),
+      "..."
     ],
   },
-};
+
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+});
